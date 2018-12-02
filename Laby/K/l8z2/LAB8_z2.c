afed9cf98f395f509af6f90f4a74682f1c12fe28
Konrad Kucharczyk <next.kuch@gmail.com>
2018-12-02 12:32:40 +0100

    l8z2 popr

--- Laby/K/l8z2/LAB8_z2.c
+++ Laby/K/l8z2/LAB8_z2.c
@@ -1,5 +1,5 @@
 //L8_z2
-
+//while((smietnik=getchar())!='\n');
 #include <stdio.h>
 #include <string.h>
 #define N 10
@@ -12,27 +12,30 @@ void zapisz_do_pliku_text(char *, double (*)[N]);
 int main(int argc, char **argv)
 {
 	char nazwa[N],nazwa2[N], tekstowy[]="r", binarny[]="rb", smietnik;
-	int i=0, j=0;
+	int i=0, j=0, len;
 	double DANE[N][N]={0.0};
+	
 	generuj_dane(DANE);
-	for (j=0; j<=9;j++)	
-		
-		for (i=0; i<=9;i++)
-			{
-				printf("%lf ",*(*(DANE+j)+i));
-			}
+
 	printf("\npodaj nazwe pliku do ktorego zapisac w formie tekstowej: \n");
 	fgets(nazwa,10,stdin);
-	zapisz_do_pliku_text(nazwa,DANE);
-	printf("\npodaj nazwe pliku do ktorego zapisac w formie binarnej: \n");
-	//while((smietnik=getchar())!='\n');
-	fgets(nazwa2,10,stdin);
-	//while((smietnik=getchar())!='\n');
-	zapisz_do_pliku_bin(nazwa2,DANE);
 	
-	printf("tekstowy \n");
+	len=strlen(nazwa); 
+	if(nazwa[len-1]=='\n') nazwa[len-1]='\0';
+	strcat (nazwa, ".txt");
+	
+	zapisz_do_pliku_text(nazwa,DANE);
+	printf("\npodaj nazwe pliku do ktorego zapisac w formie binarnej: \n");	
+	fgets(nazwa2,10,stdin);	
+	
+	len=strlen(nazwa2); 
+	if(nazwa2[len-1]=='\n') nazwa2[len-1]='\0';
+	strcat (nazwa2, ".txt");
+	
+	zapisz_do_pliku_bin(nazwa2,DANE);	
+	printf("tekstowy %s\n ",nazwa);
 	wyswietl(nazwa,tekstowy);
-	printf("\nbinarny \n");
+	printf("\nbinarny %s\n",nazwa2);
 	wyswietl(nazwa2,binarny);
 	
 	return 0;
@@ -53,7 +56,7 @@ void zapisz_do_pliku_bin(char *plik, double (*t)[N])
 	{
 		int i, j;
 		FILE *fp;
-		fp=fopen(plik,"wb");
+		fp=fopen(plik,"w+b");
 		if (fp==NULL) fprintf(stderr,"Blad otwarcia pliku\n");
 		else 
 			{
