2efd58393687d176fd13bd83b1a680ecdd307166
Konrad Kucharczyk <next.kuch@gmail.com>
2018-12-02 11:52:43 +0100

    stary dlaczego core dump kiedy inna nazwa niz plik1.txt przetestuj

--- Laby/K/Lab8_1.c
+++ Laby/K/Lab8_1.c
@@ -9,13 +9,14 @@ int wczytaj(char (*), char (*)[M]);
 
 int main()
 	{
-		char nazwa[]="plik1";
-		printf("START\n\n");
+		int x;
+		char nazwa[M];
+		printf("START\n\nPodaj nazwe pliku ktory chcesz otworzyc:");
 		char tab1[N][M];
-		wczytaj(nazwa, tab1);
-		wyswietl(tab1, N);
-		printf("\n\nSTOP\n\n");
-		//printf("\n\n%d\n\n",wczytaj(nazwa, tab1));
+		fgets(nazwa, 10, stdin);
+		x=wczytaj(nazwa, tab1);
+		if (x!=-1) wyswietl(tab1, N);
+		printf("\n");
 		
 		return 0;
 	}
@@ -25,28 +26,30 @@ int wczytaj(char *plik1, char (*tablica)[M])
 		int i=0, j=0, ret;
 		char strzpliku[L];
 		FILE *fp;
-		fp=fopen("plik1.txt","r");
-		for(i=0; i<=100000000; i++) 
-			{
-				strzpliku[i] = fgetc(fp);
-				if( feof(fp) ) break;
-			  
-			}
-		/////fscanf(fp, "%c", strzpliku);
-		//printf("\n%s\n\n",strzpliku);
-		for(i=0; i<=strlen(strzpliku); i++)
-			{
-				int j=0;
-				if (*(strzpliku+i)!='\n') *(*(tablica+j)+i)=*(strzpliku+i);
-				else if (*(strzpliku+i)=='\n') 
-					{
-						*(*(tablica+j)+i)=*(strzpliku+i);
-						j++;
-					}
-			}
-		
+		fp=fopen(plik1,"r");
 		if (fp==NULL) ret=-1;
-		else ret=0;
+		else
+			{	
+					for(i=0; i<=100000000; i++) 
+						{
+							strzpliku[i] = fgetc(fp);
+							if( feof(fp) ) break;
+						  
+						}
+					
+					for(i=0; i<=strlen(strzpliku); i++)
+						{
+							int j=0;
+							if (*(strzpliku+i)!='\n') *(*(tablica+j)+i)=*(strzpliku+i);
+							else if (*(strzpliku+i)=='\n') 
+								{
+									*(*(tablica+j)+i)=*(strzpliku+i);
+									j++;
+								}
+						}
+					ret=0;
+			}
+		printf("%d",ret);
 		fclose(fp);
 		return ret;
 	}
