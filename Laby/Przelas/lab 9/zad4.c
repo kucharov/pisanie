5c919de61ecd09f27c6047f669a0c42e536fda2c
przelas <przelasek@gmail.com>
2018-12-19 23:06:44 +0100

    no

--- Laby/Przelas/lab 9/zad4.c
+++ Laby/Przelas/lab 9/zad4.c
@@ -37,6 +37,7 @@ return nowykoniec;
 struct fragment *nowypoczatek(struct fragment *ostatninastepniak, int cyfra);
 {
 	struct fragment *nowypoczatek = malloc(sizeof(struct fragment));
+	if(ostatninastepniak ==NULL) {nowypoczatek->wartosc = cyfra; return nowypoczatek;}
 	if (nowypoczatek==0) {printf("\nZjebalo sie przy dodawaniu elementu"); exit(2);}
 	
 	nowypoczatek->poprzedni = NULL;
@@ -64,6 +65,91 @@ printf("Fragment[%d]     =    %d\n", liczydlo, aktualniak->wartosc);
 aktualniak=aktualniak->nastepny;
 }
 }
+
+struct fragment *usuwanie(struct fragment *doskasowania);
+{
+struct fragment *poprzedniak=doskasowania->poprzedni, *nastepniak=doskasowania->nastepny;
+
+if(poprzedniak==NULL && (nastepniak==NULL)) return NULL;
+
+if(poprzedniak==NULL) {nastepniak->poprzedni = NULL; free(doskasowania); return nastepniak;} 
+
+if(nastepniak==NULL) {struct fragment *aktualniak=doskasowania;
+						while(aktualniak->poprzedniak!=NULL) aktualniak = aktualniak->poprzedni; poprzedniak->nastepny = NULL;
+						  free(doskasowania); ; return aktualniak;}
+
+struct fragment *aktualniak = doskasowania;
+while(aktualniak!=NULL) aktualniak = aktualniak->poprzedniak;
+
+nastepniak->poprzedni = poprzedniak; poprzedniak->nastepny = nastepniak;
+free(doskasowania);
+
+
+return aktualniak;
+}
+
+
+void zwolnienie(struct fragment *pierwszyelement)
+{
+	struct fragment *schowek;
+	
+while (pierwszyelement->nastepny != NULL)
+{
+	
+	schowek = pierwszyelement->nastepny;
+	free(schowek->poprzedni);
+	pierwszyelement = schowek;
+	
+	if (pierwszyelement->nastepny == NULL) free(pierwszyelement);
+}
+	
+	
+}
+
+
+
+int main(void)
+{
+		struct fragment *pierwszy;
+		char odp;
+																																												powrotpobledzie:
+		printf ("Witaj w liscie dwukierunkowej, wcisnij:\n'a' - nowy fragment na poczatku\n'b' - nowy fragment na koncu\n'c' - wyswietl liste\n'd' - usun fragment\n'e' - usun cala liste\n :  ");
+		scanf("%c", &odp);
+		
+		switch(odp)
+			case 'a': pierwszy= nowypoczatek(
+				
+			
+			
+			case 'b': strasznie duzo pierdolenia z tym nie chce mi sie ide spac zZZzzZZZzZZ
+			
+			
+			case 'c': cipa
+			
+			
+			case 'd': dupa
+			
+			
+			case 'e':
+			
+			
+			default: printf("\n Blad wprowadzania, powracam do menu.\n\n\n\n"); goto powrotpobledzie;
+		
+
+
+
+	
+}
+
+
+
+
+	
+
+
+
+
+
 
 
 
